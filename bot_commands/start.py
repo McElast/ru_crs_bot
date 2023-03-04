96a02a31620641e995d984d20bc35425d307b1ac
makarik.m <makkee@mail.ru>
2023-03-04 14:41:42 +0400

    расширил список команд

--- bot_commands/start.py
+++ bot_commands/start.py
@@ -9,9 +9,17 @@ logger = log_configured.getLogger(__name__)
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Логика команды /start."""
     if update.effective_chat is not None:
+        start_message: str = (
+            f'Показываю курс рубля специально для тебя, {update.effective_chat.first_name}.'
+            '\n======Доступные команды======\n'
+            '- /start - приветственная информация\n'
+            '- /courses - доступные курсы валют'
+            '- /mine - показать мои курсы валют'
+        )
+
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
-            text=f'Показываю курс рубля специально для тебя, {update.effective_chat.first_name}.',
+            text=start_message,
         )
     else:
         logger.warning('Не получен ID чата при запросе /start.')
